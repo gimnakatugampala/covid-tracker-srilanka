a76cd9dbc3a82e8aa8a1a13b144980b16dbe7845
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-06-12 21:41:03 +0530

    img

--- dist/main.fb6bbcaf.js
+++ dist/main.fb6bbcaf.js
@@ -172,10 +172,47 @@ function getDailyLocal() {
     data: data
   };
   var myChart = new Chart(document.getElementById('getDaily'), config);
+} // get the total cases world
+
+
+function getTotalWorld() {
+  var data = {
+    labels: ['Red', 'Blue', 'Yellow'],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 50, 100],
+      backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
+      hoverOffset: 4
+    }]
+  };
+  var config = {
+    type: 'pie',
+    data: data
+  };
+  var myChart = new Chart(document.getElementById('getTotalWorld'), config);
+}
+
+function getDailyWorld() {
+  var data = {
+    labels: ['Red', 'Blue', 'Yellow'],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 50, 100],
+      backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
+      hoverOffset: 4
+    }]
+  };
+  var config = {
+    type: 'pie',
+    data: data
+  };
+  var myChart = new Chart(document.getElementById('getDailyWorld'), config);
 }
 
 getTotalLocal();
 getDailyLocal();
+getTotalWorld();
+getDailyWorld();
 },{}],"C:/Users/User/AppData/Roaming/npm/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- js/main.js
+++ js/main.js
@@ -89,5 +89,73 @@ function getDailyLocal(){
 
 }
 
+// get the total cases world
+function getTotalWorld(){
+
+    const data = {
+        labels: [
+          'Red',
+          'Blue',
+          'Yellow'
+        ],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [300, 50, 100],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+          ],
+          hoverOffset: 4
+        }]
+      };
+  
+  const config = {
+    type: 'pie',
+    data: data,
+  };
+
+  var myChart = new Chart(
+    document.getElementById('getTotalWorld'),
+    config
+  );
+    
+}
+
+function getDailyWorld(){
+
+    const data = {
+        labels: [
+          'Red',
+          'Blue',
+          'Yellow'
+        ],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [300, 50, 100],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+          ],
+          hoverOffset: 4
+        }]
+      };
+  
+  const config = {
+    type: 'pie',
+    data: data,
+  };
+
+  var myChart = new Chart(
+    document.getElementById('getDailyWorld'),
+    config
+  );
+  
+
+}
+
 getTotalLocal()
 getDailyLocal()
+getTotalWorld()
+getDailyWorld()
