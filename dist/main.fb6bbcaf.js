fffbc570e64b64b864e75f5893a12243a7339367
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-06-12 18:14:02 +0530

    added the chart

--- dist/main.fb6bbcaf.js
+++ dist/main.fb6bbcaf.js
@@ -153,10 +153,29 @@ function getTotalLocal() {
     type: 'pie',
     data: data
   };
-  var myChart = new Chart(document.getElementById('myChart'), config);
+  var myChart = new Chart(document.getElementById('getTotal'), config);
+} // Get Daily local
+
+
+function getDailyLocal() {
+  var data = {
+    labels: ['Red', 'Blue', 'Yellow'],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 50, 100],
+      backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
+      hoverOffset: 4
+    }]
+  };
+  var config = {
+    type: 'pie',
+    data: data
+  };
+  var myChart = new Chart(document.getElementById('getDaily'), config);
 }
 
 getTotalLocal();
+getDailyLocal();
 },{}],"C:/Users/User/AppData/Roaming/npm/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- js/main.js
+++ js/main.js
@@ -49,10 +49,45 @@ function getTotalLocal(){
   };
 
   var myChart = new Chart(
-    document.getElementById('myChart'),
+    document.getElementById('getTotal'),
     config
   );
 
 }
 
+// Get Daily local
+function getDailyLocal(){
+
+    const data = {
+        labels: [
+          'Red',
+          'Blue',
+          'Yellow'
+        ],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [300, 50, 100],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+          ],
+          hoverOffset: 4
+        }]
+      };
+  
+  const config = {
+    type: 'pie',
+    data: data,
+  };
+
+  var myChart = new Chart(
+    document.getElementById('getDaily'),
+    config
+  );
+
+
+}
+
 getTotalLocal()
+getDailyLocal()
